711bc488cb9dda38b39ec0ba15dcef4cec2520ea
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2021-08-22 19:02:20 -0400

    test: add tests for index, profile routes

---
+++ tests/acceptance/authenticated/profile-test.js
@@ -0,0 +1,37 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { currentSession } from 'ember-simple-auth/test-support';
+import AUTH from 'ember-base-template/mirage/constants/auth';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
+module('Acceptance | user/profile', function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('visiting /user/profile redirects to /login if user is not logged in', async function (assert) {
+    await visit('/user/profile');
+
+    assert.equal(currentURL(), '/login', 'redirects to /login');
+  });
+
+  test('visiting /user/profile if user is logged in', async function (assert) {
+    const user = this.server.create('user', AUTH.loginIdentificationObject);
+    await visit('/user/profile');
+
+    await fillIn('[data-test-input="email"]', user.email);
+    await fillIn('[data-test-input="current-password"]', user.currentPassword);
+    await click('[data-test-action="login"]');
+
+    const sessionData = currentSession().get('data.authenticated');
+
+    assert.deepEqual(
+      sessionData,
+      AUTH.loginRequestTokenObject,
+      'session data is set'
+    );
+
+    // FIXME: this should flow through to /user/profile route
+    assert.equal(currentURL(), '/', 'user is redirected to index after login');
+  });
+});
