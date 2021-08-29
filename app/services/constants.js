ceefcb20fd4c247f1b1eb4364a3adabcace62dc6
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2021-08-29 06:24:42 -0400

    refactor: move icons into constants, dark mode functions to service

--- app/services/constants.js
+++ app/services/constants.js
@@ -11,4 +11,7 @@ export default class ConstantsService extends Service {
   DEFAULT_LOCALE = Object.freeze('en-us');
   DEFAULT_DARK_MODE = Object.freeze(true);
   RTL_LANGUAGES = Object.freeze(['ar']);
+  ICON_X = Object.freeze('M6 18L18 6M6 6l12 12');
+  ICON_HAMBURGER = Object.freeze('M4 6h16M4 12h16M4 18h16');
+  ICON_MOON = Object.freeze('M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z');
 }
--- app/services/dark-mode.js
+++ app/services/dark-mode.js
@@ -1,13 +1,24 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class DarkModeService extends Service {
   @service session;
   @service constants;
 
+  @tracked enabled =
+    this.session.data.darkMode || this.constants.DEFAULT_DARK_MODE;
+
   @action
-  async setDarkMode(value) {
-    await this.session.set('data.darkMode', value);
+  toggleDarkMode() {
+    const shouldBeDarkMode = !this.enabled;
+
+    this.enabled = shouldBeDarkMode;
+  }
+
+  @action
+  setDarkMode(value) {
+    this.session.data.darkMode = value;
   }
 }
