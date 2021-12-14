471a507ecf66ba77bf783c523bb468829396edf2
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2021-12-13 22:27:46 -0500

    refactor: redirect to profile

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class SignupRoute extends Route {}
+export default class SignupRoute extends Route {
+  @service session;
+
+  beforeModel() {
+    this.session.prohibitAuthentication('authenticated.profile');
+  }
+}
